53585b742ddb15115ae542267a3815166a8fa0be
DmitryAlCh <dmi.al.ch@gmail.com>
2017-04-10 16:04:54 +0300

    node chat app initialization

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+  console.log(`Server is Up and running on ${port}`);
+});
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
