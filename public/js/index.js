699ec1f574436aa88eabf01b8fe30ea51b4aca9d
DmitryAlCh <dmi.al.ch@gmail.com>
2017-07-13 17:21:19 +0300

    add newMessage and createMessage events

--- public/js/index.js
+++ public/js/index.js
@@ -7,15 +7,12 @@ var clientMessage = {
 socket.on('connect',function() {
   console.log('Conneced to server');
   socket.emit('createMessage',clientMessage);
+
 });
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function(email) {
-  console.log('New Email', email);
-});
-
 socket.on('newMessage', function(message) {
-  console.log('New chat message', message);
+  console.log('NewMessage', message);
 });
